47a89323e65a22ab0a36ae489c39f8cea4416089
Robbie Veraldi <robbie.veraldi@gmail.com>
2022-09-01 14:55:37 -0400

    added content to seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,45 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Song.create!(
+  title: "LVL",
+  album: "LONG.LIVE.A$AP"
+  artist: "A$AP Rocky",
+  year: "2013"
+)
+
+Song.create!(
+  title: "Rap Saved Me",
+  album: "Without Warning"
+  artist: "21 Savage, Offset, Metro Boomin",
+  year: "2017"
+)
+
+Song.create!(
+  title: "Pris Morton Music",
+  album: "Care Package"
+  artist: "Drake",
+  year: "2019"
+)
+
+Song.create!(
+  title: "This Could Be Us",
+  album: "SremmLife"
+  artist: "Rae Sremmurd",
+  year: "2015"
+)
+
+Song.create!(
+  title: "Maria I'm Drunk",
+  album: "Rodeo"
+  artist: "Travis Scott, Justin Bieber, Young Thug",
+  year: "2015"
+)
+
+Song.create!(
+  title: "Dior (Remix)",
+  album: "Meet The Woo 2"
+  artist: "Pop Smoke, Gunna",
+  year: "2020"
+)
