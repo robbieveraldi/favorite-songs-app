3abdc9b8c45f1654089eee2a3cb04dd0e8099695
Robbie Veraldi <robbie.veraldi@gmail.com>
2022-09-01 14:46:12 -0400

    created create, update, and destroy methods and routes

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,11 +1,37 @@
 class SongsController < ApplicationController
   def index
     songs = Song.all
-    render json: songs
+    render json: songs.as_json
   end
 
   def show
-    # song = Song.find_by(id: params["id"])
-    render json: song
+    song = Song.find_by(id: params["id"])
+    render json: song.as_json
+  end
+
+  def create
+    song = Song.new(
+      year: params["title"],
+      artist: params["artist"],
+      album: params["album"],
+      year: params["year"],
+    )
+    song.save
+    render json: song.as_json
+  end
+
+  def update
+    song = Songs.find_by(id: params["id"])
+    song.title = params["title"] || song.title
+    song.artist = params["artist"] || song.artist
+    song.album = params["album"] || song.album
+    song.year = params["year"] || song.year
+    render json: song.as_json
+  end
+
+  def destroy
+    song = Song.find_by(id: params["id"])
+    song.destroy
+    render json: { message: "Song Destroyed" }
   end
 end
