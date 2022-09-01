7f04b8d76b2198217c24cbe42e7e541bba7047a6
Robbie Veraldi <robbie.veraldi@gmail.com>
2022-09-01 14:14:05 -0400

    created index and show routes and actions

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,2 +1,11 @@
 class SongsController < ApplicationController
+  def index
+    songs = Song.all
+    render json: songs
+  end
+
+  def show
+    # song = Song.find_by(id: params["id"])
+    render json: song
+  end
 end
